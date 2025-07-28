5b223fd34d61df438293a0460a880f3b1242b49e
Okay Padak <kypdk@proton.me>
2025-07-28 15:44:35 +0300

    env için devam

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,15 @@ import { NotificationModule } from './notification/notification.module';
 import { ProjectModule } from './project/project.module';
 import { CustomerModule } from './customer/customer.module';
 
+console.log('✅ ENV VALUES ------------------');
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASS:', process.env.DB_PASS);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_SSL:', process.env.DB_SSL);
+console.log('--------------------------------');
+
 @Module({
   imports: [
     ConfigModule.forRoot({
