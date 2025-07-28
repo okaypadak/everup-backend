f703b08491947f68bfa054c2dc16932f61a52e79
Okay Padak <kypdk@proton.me>
2025-07-28 14:36:25 +0300

    admin

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,36 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './user.entity';
+import { User, UserRole } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {
+export class UserService implements OnModuleInit {
   constructor(
     @InjectRepository(User)
     private readonly userRepo: Repository<User>
   ) {}
+
+  async onModuleInit() {
+    const adminExists = await this.userRepo.findOne({ where: { role: 'ADMIN' } });
+
+    if (!adminExists) {
+      const adminUser: CreateUserDto = {
+        username: 'admin',
+        email: 'kypdkz@gmail.com',
+        password: 'padaK06##',
+        role: UserRole.ADMIN,
+        firstName: 'Okay',
+        lastName: 'Padak',
+      };
+
+      await this.create(adminUser);
+      console.log('✅ Admin kullanıcısı oluşturuldu');
+    } else {
+      console.log('ℹ️ Admin zaten mevcut');
+    }
+  }
 
   async create(dto: CreateUserDto): Promise<User> {
     const passwordHash = await bcrypt.hash(dto.password, 10);
@@ -19,19 +39,5 @@ export class UserService {
       password: passwordHash,
     });
     return this.userRepo.save(user);
-  }
-
-  async findById(id: number): Promise<User> {
-    const user = await this.userRepo.findOne({ where: { id } });
-    if (!user) throw new NotFoundException('Kullanıcı bulunamadı');
-    return user;
-  }
-
-  async findByEmail(email: string): Promise<User | null> {
-    return this.userRepo.findOne({ where: { email } });
-  }
-
-  async findByUsername(username: string): Promise<User | null> {
-    return this.userRepo.findOne({ where: { username } });
   }
 }
