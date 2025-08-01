b560b15b386e813fbc6e2058f830e24bf9719c85
Okay Padak <kypdk@proton.me>
2025-08-01 20:53:48 +0300

    hata giderme

--- src/comment/dto/response-comment.dto.ts
+++ src/comment/dto/response-comment.dto.ts
@@ -1,15 +1,14 @@
+// src/comment/dto/response-comment.dto.ts
+import { Comment } from '../comment.entity';
+
 export class ResponseCommentDto {
   id: number;
   text: string;
   createdAt: Date;
-  authorName: string;
-  taskTitle: string;
 
   constructor(comment: Comment) {
     this.id = comment.id;
     this.text = comment.content;
     this.createdAt = comment.createdAt;
-    this.authorName = comment.author?.name;
-    this.taskTitle = comment.task?.title;
   }
 }
